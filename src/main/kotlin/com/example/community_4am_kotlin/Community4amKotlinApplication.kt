2d1fc57b8e7761ad1966e7f90b31a8fb352b84f3
MagongDo <lpok2401@naver.com>
2024-11-13 16:46:12 +0900

    CI 추가

--- src/main/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplication.kt
+++ src/main/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplication.kt
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
--- src/test/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplicationTests.kt
@@ -2,12 +2,13 @@ package com.example.community_4am_kotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(locations = ["classpath:/application-dev.properties"])
 class Community4amKotlinApplicationTests {
     @Test
     fun contextLoads() {
