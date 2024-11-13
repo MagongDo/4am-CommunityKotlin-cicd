a0fe30f5872fb9e99297fc59beeb9dfeeff4ad8f
MagongDo <lpok2401@naver.com>
2024-11-13 17:32:37 +0900

    CI 추가

--- src/test/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplicationTests.kt
@@ -1,20 +1,14 @@
 package com.example.community_4am_kotlin
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Import
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(locations = ["classpath:/application-dev.properties"])
+@TestPropertySource(locations = ["classpath:/application-dev.yml"])
 class Community4amKotlinApplicationTests {
     @Test
     fun contextLoads() {
