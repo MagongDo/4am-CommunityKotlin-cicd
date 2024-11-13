2b4f37c516343bff78f847704e9fd1e172b26a07
MagongDo <lpok2401@naver.com>
2024-11-13 16:06:58 +0900

    CI 추가

--- src/test/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/community_4am_kotlin/Community4amKotlinApplicationTests.kt
@@ -2,8 +2,10 @@ package com.example.community_4am_kotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
+@ActiveProfiles("test")
 @SpringBootTest
 @TestPropertySource(locations = ["classpath:/application-test.yml"])
 
